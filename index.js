176446300fc4eb21585943af950c7b4c83ff3dbf
이름 <>
2020-12-22 10:19:29 +0800

    bot upload

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ client.on("message", (message) => {
   if (message.content == "!봇건의"){
     return message.reply("봇 에러 또는 추가하고 싶은게 있으면 말해주세요! = https://discord.gg/4QqNp3jxWC") 
   }
-
+ 
 
   if (message.content == '!얼불춤 포럼'){
     return message.reply("얼불춤 포럼 도움말 `(참고) 본 포럼은 얼불춤 비공식 포럼에서 가져온 자료입니다.` 사용법 ```!얼불춤 {곡 이름}``` {작곡가를 제외한 곡이름만 작성해주세요} (예시 `!얼불춤 we could get more machinegun psystyle` (예시2 `!얼불춤 tripple cross` `대문자 제외하고 소문자로 써주세요.` `[ns] 처럼 대괄호나 소괄호 도 작성해주세요`")
@@ -201,38 +201,82 @@ if (message.content == "!얼불춤 Shiver") {
 }
     
 
-  if (message.content.startsWith("!공지")) {
-    if (checkPermission(message)) return
-    if (message.member != null) {
-      let contents = message.content.slice("!공지".length)
-      message.member.guild.members.cache.array().forEach((x) => {
-        if (x.user.bot) return
-        x.user.send(`<@${message.author.id}> ${contents}`)
-      })
+if (message.content.startsWith("!전체공지")) {
+  if (checkPermission(message)) return
+  if (message.member != null) {
+    // 채널에서 공지 쓸 때
+    let contents = message.content.slice("!전체공지".length)
+    message.member.guild.members.cache.array().forEach((x) => {
+      if (x.user.bot) return
+      x.user.send(`<@${message.author.id}> ${contents}`)
+    })
 
-      return message.reply("우왕 새로운 공지다 :smile:")
-    } else {
-      return message.reply("채널에서 실행해주세요.")
-    }
+    return message.reply("공지를 전송했습니다.")
+  } else {
+    return message.reply("채널에서 실행해주세요.")
   }
+}
+
+if (message.content.startsWith("!청소")) {
+  if (message.channel.type == "dm") {
+    return message.reply("dm에서 사용할 수 없는 명령어 입니다.")
+  }
+
+  if (message.channel.type != "dm" && checkPermission(message)) return
+
+  var clearLine = message.content.slice("!청소 ".length)
+  var isNum = !isNaN(clearLine)
+
+  if (isNum && (clearLine <= 0 || 100 < clearLine)) {
+    message.channel.send("1부터 100까지의 숫자만 입력해주세요.")
+    return
+  } else if (!isNum) {
+    // c @나긋해 3
+    if (message.content.split("<@").length == 2) {
+      if (isNaN(message.content.split(" ")[2])) return
+
+      var user = message.content.split(" ")[1].split("<@!")[1].split(">")[0]
+      var count = parseInt(message.content.split(" ")[2]) + 1
+      let _cnt = 0
+
+      message.channel.messages.fetch().then((collected) => {
+        collected.every((msg) => {
+          if (msg.author.id == user) {
+            msg.delete()
+            ++_cnt
+          }
+          return !(_cnt == count)
+        })
+      })
+    }
+  } else {
+    message.channel
+      .bulkDelete(parseInt(clearLine) + 1)
+      .then(() => {
+        message.channel.send(`<@${message.author.id}> ${parseInt(clearLine)} 개의 메시지를 삭제했습니다. (이 메시지는 잠시 후 사라집니다.)`).then((msg) => msg.delete({ timeout: 3000 }))
+      })
+      .catch(console.error)
+  }
+}
 })
 
 function checkPermission(message) {
-  if (!message.member.hasPermission("MANAGE_MESSAGES")) {
-    message.channel.send(`<@${message.author.id}> 명령어를 수행할 관리자 권한을 소지하고 있지않습니다.`)
-    return true
-  } else {
-    return false
-  }
+if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+  message.channel.send(`<@${message.author.id}> 명령어를 수행할 관리자 권한을 소지하고 있지않습니다.`)
+  return true
+} else {
+  return false
 }
+}
+
 function changeCommandStringLength(str, limitLen = 8) {
-  let tmp = str
-  limitLen -= tmp.length
+let tmp = str
+limitLen -= tmp.length
 
-  for (let i = 0; i < limitLen; i++) {
-    tmp += " "
-  }
+for (let i = 0; i < limitLen; i++) {
+  tmp += " "
+}
 
-  return tmp
+return tmp
 }
 client.login(token)
